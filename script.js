7117fffb036a1d1facf0b212836e04775c925764
TyPlath <tyler_plath@comcast.com>
2022-07-26 15:04:40 -0400

    validate fields

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 // Write your JavaScript code here!
 
+
+//1. Set up window load handler
+window.addEventListener("load", function(){
+
+//2. Set up submit handler.
+const button = document.getElementById("formSubmit");
+
+//3. event.preventDefault()
+button.addEventListener("click"), function(event) {
+    event.preventDefault();
+}
+//4. All inputs have data.
+
+button.addEventListener("click", function() {
+         const pilotName = document.getElementById("pilotName");
+         const copilotName = document.getElementById("copilotName");
+         const fuelLevel = document.getElementById("fuelLevel");
+         const cargoMass = document.getElementById("cargoMass");
+
+         if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === ""){
+            alert("You must enter every field.");
+         }
+
+      });
+});
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ul>
