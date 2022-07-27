1221e2c25dcf8b8c80718559cee0cddab807d588
TyPlath <tyler_plath@comcast.com>
2022-07-27 14:13:42 -0400

    final validation commit

--- script.js
+++ script.js
@@ -18,6 +18,12 @@ window.addEventListener("load", function(){
          const copilotName = document.getElementById("copilotName");
          const fuelLevel = document.getElementById("fuelLevel");
          const cargoMass = document.getElementById("cargoMass");
+         const pilotStatus = document.getElementById("pilotStatus");
+         const copilotStatus = document.getElementById("copilotStatus");
+         const fuelStatus = document.getElementById("fuelStatus");
+         const cargoStatus = document.getElementById("cargoStatus");
+         const launchStatus = document.getElementById("launchStatus");
+         const itemStatus = document.getElementById("itemStatus");
 
          if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === ""){
             alert('You must enter every field.');
@@ -31,6 +37,36 @@ window.addEventListener("load", function(){
          } else if (isNaN(cargoMass.value)) {
             alert('Please enter cargo mass using numbers only.');
          }
+
+         pilotStatus.innerHTML = `Our pilot, ${pilotName.value}, is ready.`;
+         copilotStatus.innerHTML = `Our co-pilot, ${copilotName.value}, is ready.`;
+         launchStatus.style.color = "red";
+         launchStatus.innerHTML = 'Shuttle not ready for launch.';
+         
+         if ((fuelLevel.value) < 10000 || cargoMass.value === "") {
+            fuelStatus.innerHTML = `Not enough fuel.`;
+            launchStatus.style.color = "red";
+            launchStatus.innerHTML = `Shuttle not ready for launch`;
+         }else if (Number(cargoMass.value) > 10000) {
+            cargoStatus.innerHTML = `Payload too heavy.`;
+            launchStatus.style.color = "red";
+            launchStatus.innerHTML = `Shuttle not ready for launch`;
+            itemStatus.style.visibility = "visible";
+         } else if (pilotName.value === "" || copilotName.value === "" || Number(pilotName.value) === 'number' || Number(copilotName.value) === 'number'){
+            launchStatus.style.color = "red";
+            launchStatus.innerHTML = `Shuttle not ready for launch`;
+            itemStatus.style.visibility = "hidden";
+         } else if (typeof(pilotName.value) === 'string' && typeof(copilotName.value) === 'string' && typeof(Number(fuelLevel.value)) === 'number' && typeof(Number(cargoMass.value)) === 'number'){
+         launchStatus.style.color = "green";
+         launchStatus.innerHTML = `Shuttle is ready for launch`;
+         itemStatus.style.visibility = "visible";
+         };
+         if (fuelLevel.value > 10000) {
+            fuelStatus.innerHTML = "Fuel levels optimal."
+         };
+         if (cargoMass.value < 10000) {
+            cargoStatus.innerHTML = "Payload within weight range."
+         };
 
       });
 });
