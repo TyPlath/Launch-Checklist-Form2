b4a7af0327fbbfc9831221967700e73208789525
TyPlath <tyler_plath@comcast.com>
2022-07-27 10:46:56 -0400

    validation updated

--- script.js
+++ script.js
@@ -4,29 +4,29 @@
 //1. Set up window load handler
 window.addEventListener("load", function(){
 
-//2. Set up submit handler.
-const button = document.getElementById("formSubmit");
+    //2. Set up submit handler.
+    const button = document.getElementById("submitForm");
 
-//3. event.preventDefault()
-button.addEventListener("click"), function(event) {
+   //3. event.preventDefault()
+   button.addEventListener("click", function(event) {
     event.preventDefault();
-}
-//4. All inputs have data.
+   });
+   //4. All inputs have data.
 
-button.addEventListener("click", function() {
+   button.addEventListener("click", function() {
          const pilotName = document.getElementById("pilotName");
          const copilotName = document.getElementById("copilotName");
          const fuelLevel = document.getElementById("fuelLevel");
          const cargoMass = document.getElementById("cargoMass");
 
          if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === ""){
-            alert("You must enter every field.");
+            alert('You must enter every field.');
          }
          if (!isNaN(pilotName.value) || pilotName.value === "") {
-            alert('Please enter a valid name using letters only.');
-         } else if (!isNaN(copilotName.value) || copilotName.value === ""){
-            alert('Please enter a valid name using letters only.');
-         } else if (isNaN(fuelLevel)) {
+            alert('Please enter a valid pilot name using letters only.');
+         } else if (!isNaN(copilotName.value)){
+            alert('Please enter a valid co-pilot name using letters only.');
+         } else if (isNaN(fuelLevel.value)) {
             alert('Please enter fuel level using numbers only.');
          } else if (isNaN(cargoMass.value)) {
             alert('Please enter cargo mass using numbers only.');
